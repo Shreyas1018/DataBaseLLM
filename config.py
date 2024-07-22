677cfb81e193dc2a01970b5741535ac8ee840822
Foxie <shreyasdomde1@gmail.com>
2024-07-22 23:08:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-hugghing_api = "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"
-pinecone_api = "a90c542e-590b-4125-af8e-d3776c366653"
+hugghing_api = "enter here your hugging face api"
+pinecone_api = "enter here your pine api"
